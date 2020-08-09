f791cdf4a73d6a5373b991aad760aef28e654667
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-09 20:26:11 +0300

    Updates

---
+++ precache-manifest.e52cd7487683bd1dd4e92648f0a2f228.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df1440a0b24f520738d4df471fb3882",
+    "url": "/jebitok-dev/chat-app/index.html"
+  },
+  {
+    "revision": "eb4051b2885eae4d04ea",
+    "url": "/jebitok-dev/chat-app/static/css/2.e05706cb.chunk.css"
+  },
+  {
+    "revision": "fc6ad00c8266d8e19009",
+    "url": "/jebitok-dev/chat-app/static/css/main.6c15062c.chunk.css"
+  },
+  {
+    "revision": "eb4051b2885eae4d04ea",
+    "url": "/jebitok-dev/chat-app/static/js/2.61887233.chunk.js"
+  },
+  {
+    "revision": "928c699a3ab8fb7985070888bee3065a",
+    "url": "/jebitok-dev/chat-app/static/js/2.61887233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6ad00c8266d8e19009",
+    "url": "/jebitok-dev/chat-app/static/js/main.6cc23cd4.chunk.js"
+  },
+  {
+    "revision": "e23393443e7c95a83a32",
+    "url": "/jebitok-dev/chat-app/static/js/runtime-main.0390e11e.js"
+  },
+  {
+    "revision": "ae68a6da3633369482b25a3ad0836885",
+    "url": "/jebitok-dev/chat-app/static/media/1.ae68a6da.jpg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/jebitok-dev/chat-app/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/jebitok-dev/chat-app/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/jebitok-dev/chat-app/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/jebitok-dev/chat-app/static/media/notification.c0d3c94c.eot"
+  }
+]);
